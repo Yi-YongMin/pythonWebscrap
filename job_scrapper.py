d617e67e65da300d19094fc871708c9e67db1fc0
Yi-YongMin <96493001+Yi-YongMin@users.noreply.github.com>
2023-01-03 17:59:57 +0900

    exception of "view all"
    view all 이 리스트 마지막에 오는 것을 인지하고, pop을 이용하여 제외.

--- job_scrapper.py
+++ job_scrapper.py
@@ -7,4 +7,11 @@ response=get(f"{base_url}{search_term}") # 위에 두 문자열을 합치는 작
 if response.status_code !=200:
     print("Can't request website")
 else:
-    print(response.text)
+    soup=BeautifulSoup(response.text,"html.parser")
+    jobs=soup.find_all('section',class_="jobs") #class_="jobs" 를 이용하는 이유는 find_all 이라는 함수가 매개변수를 너무 많이 갖고있기 때문에.
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            print(post)
+            print("//////////////////////////////////////////////////")
