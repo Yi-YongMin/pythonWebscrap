2e64a3e8ddc7addb4970600e749c9e2180772187
Yi-YongMin <96493001+Yi-YongMin@users.noreply.github.com>
2023-01-03 14:13:17 +0900

    Tip of while

--- controlFlow.py
+++ controlFlow.py
@@ -19,19 +19,18 @@
 # False and False==False #이거 디논 게이트 1*1 1*0 0*1 0*0 게이트(or)도 디논때 배운대로
 
 from random import randint,random  #random 모듈에서 randint 라는 함수를 쓰겠다는 선언
-achoice=int(input("Choose number"))
+
 bchoice= randint(1,100)
 playing=True
 while playing:
+    achoice=int(input("Choose number"))
     if achoice==bchoice:
         print("SAME")
         playing=False
     elif achoice > bchoice:
         print("당신이 선택한 숫자가 더 높음.")
-        achoice = int(input("Choose number"))
     else :
         print("당신이 선택한 숫자가 더 낮음.")
-        achoice = int(input("Choose number"))
 
 
 
