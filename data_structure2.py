2fc18149af537bbc0fa7347c2acc5fa3d5bd7463
Yi-YongMin <96493001+Yi-YongMin@users.noreply.github.com>
2023-01-03 16:46:09 +0900

    Update data_structure2.py

--- data_structure2.py
+++ data_structure2.py
@@ -12,7 +12,8 @@ for website in websites:
     if not website.startswith("https://"):
     #if website.startswith("https://")==False : 와 동치이다.
         website=f"https://{website}" #f"를 기억하자. string에 변수를 추가하는 방법이다.
-        response=get(website) # 만일 print(response)를 한다면 Response[200] 이라는 문장이 나온다. 이는 error404라는 흔히본 오류코드와 같은 이치.
+        response=get(website) #get은 우리가 URL을 받고 웹사이트를 가져올수 있게 해준다
+    # 만일 print(response)를 한다면 Response[200] 이라는 문장이 나온다. 이는 error404라는 흔히본 오류코드와 같은 이치.
         # if response.status_code==200:
         #     print(f"{website} is okay")
         # else:
