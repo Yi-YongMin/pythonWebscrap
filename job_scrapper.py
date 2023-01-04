8fb94bc985ffca5a24069b2bd7526f6afaaca77a
Yi-YongMin <96493001+Yi-YongMin@users.noreply.github.com>
2023-01-04 14:25:16 +0900

    before_flask
    플라스크 설치하기 전

--- job_scrapper.py
+++ job_scrapper.py
@@ -1,9 +1,6 @@
 from extractors.wwr import extract_wwr_jobs
+from file import save_to_file
 keyword = input("what do you want to search for?")
 wwr=extract_wwr_jobs(keyword)
 jobs= wwr
-file=open(f"{keyword}.csv","w")
-file.write("Position,Company,Location,URL\n")
-for job in jobs:
-    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
-file.close()
+save_to_file(keyword,jobs)
