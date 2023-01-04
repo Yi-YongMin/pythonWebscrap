e61b230f545d13ba75c09e3ed0a7119aa959e33e
Yi-YongMin <96493001+Yi-YongMin@users.noreply.github.com>
2023-01-04 16:21:55 +0900

    end
    flask를 이용할 것 같지는 않다.

--- job_scrapper.py
+++ job_scrapper.py
@@ -1,6 +1,9 @@
-from extractors.wwr import extract_wwr_jobs
-from file import save_to_file
-keyword = input("what do you want to search for?")
-wwr=extract_wwr_jobs(keyword)
-jobs= wwr
-save_to_file(keyword,jobs)
+from flask import Flask
+app=Flask("JobScrapper")
+@app.route("/")
+def home():
+    return 'hey'
+@app.route("/hello")
+def hello():
+    return 'hello you'
+app.run("0.0.0.0")
